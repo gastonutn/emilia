df9dc32899c0c70622b77129c4531e372503ec6f
Gaston Gomez <gaston.miguel.gomez@gmail.com>
2023-06-13 12:54:55 -0300

    estaticos

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require("path")
 const PORT = 3030
 
+app.use(express.static("public"))
+
 app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","home.html")));
 
 app.get("/about",(req,res) => res.sendFile(path.join(__dirname,"view","about.html")));
