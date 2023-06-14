b43446f1bcc13872a121aba14cd39910173f64be
Gaston Gomez <gaston.miguel.gomez@gmail.com>
2023-06-14 02:13:03 -0300

    se agrega paginas

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ app.use(express.static("public"))
 
 app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","home.html")));
 
-app.get("/about",(req,res) => res.sendFile(path.join(__dirname,"view","about.html")));
+app.get("/about",(req,res) => res.sendFile(path.join(__dirname,"views","about.html")));
 
-app.get("/contacto",(req,res) => res.sendFile(path.join(__dirname,"view","contacto.html")));
-app.get("/music",(req,res) => res.sendFile(path.join(__dirname,"view","music.html")))
-app.get("/*",(req,res) => res.sendFile(path.join(__dirname,"view","404.html")))
+app.get("/contacto",(req,res) => res.sendFile(path.join(__dirname,"views","contacto.html")));
+app.get("/music",(req,res) => res.sendFile(path.join(__dirname,"views","music.html")))
+app.get("/*",(req,res) => res.sendFile(path.join(__dirname,"views","404.html")))
 
 app.listen(PORT, () => console.log(`servidor corriendo en http://localhost:`+PORT))
 
